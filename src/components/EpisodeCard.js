683640fe723f85dca69159280b695607c1bedef2
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-07-26 13:04:02 -0400

    episode list styled woth grid

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -1,13 +1,47 @@
 import React from "react";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 300,
+    marginBottom: 20,
+    boxSizing: "border-box"
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
+  },
+  title: {
+    fontSize: 18
+  },
+  pos: {
+    marginBottom: 12
+  }
+});
 
 const EpisodeCard = ({ episode }) => {
-  console.log(episode);
+  const classes = useStyles();
   return (
-    <div>
-      <h3>Episode Name: {episode.name}</h3>
-      <p>Season: {episode.episode}</p>
-      <p>Air Date: {episode.air_date}</p>
-    </div>
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} gutterBottom>
+          {episode.name}
+        </Typography>
+        <Typography>Season: {episode.episode}</Typography>
+        <Typography>Air Date: {episode.air_date}</Typography>
+      </CardContent>
+    </Card>
+
+    // <div>
+    //   <h3>Episode Name: {episode.name}</h3>
+    //   <p>Season: {episode.episode}</p>
+    //   <p>Air Date: {episode.air_date}</p>
+    // </div>
   );
 };
 
